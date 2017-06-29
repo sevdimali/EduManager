6521263dbb807abcd0a9a44089c48a8b71f1e230
sevdimali <sevdimaliisayev@mail.ru>
2017-06-30 01:31:24 +0400

    Update configM.php

--- configM.php
+++ configM.php
@@ -1,6 +1,13 @@
 <?php 
 	$baglan = @mysql_connect("localhost", "root", "");
-	$vb_sec = @mysql_select_db("diplom",$baglan);
+	$vb_sec = @mysql_select_db("bm_reg",$baglan);
 	//mysql_query("SET CHARACTER SET utf8_general_mysql500_ci");
 	mysql_set_charset('utf8', $baglan);
+	
+	//other options
+	////////////////////////////////////////////////
+	
+	//site url example: https://sevdimali.com/
+	
+	$site_url = 'http://localhost/bm-reg/';
 ?>
