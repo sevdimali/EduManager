a0bf490941a05639132becba47845bd406adb0e3
sevdimali <sevdimaliisayev@mail.ru>
2017-06-30 01:54:45 +0400

    Update configM.php

--- configM.php
+++ configM.php
@@ -1,6 +1,6 @@
 <?php 
-	$baglan = @mysql_connect("localhost", "root", "");
-	$vb_sec = @mysql_select_db("bm_reg",$baglan);
+	$baglan = @mysql_connect("localhost", "database_username", "database_password");
+	$vb_sec = @mysql_select_db("database_name",$baglan);
 	//mysql_query("SET CHARACTER SET utf8_general_mysql500_ci");
 	mysql_set_charset('utf8', $baglan);
 	
